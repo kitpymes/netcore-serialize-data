5e6e8d90665c5887593cac68ef998672675338fd
sferrari <sferrari.net@gmail.com>
2018-12-01 15:26:03 +0000

    .

--- SerializeData/Common/Models/JsonConfig.cs
+++ SerializeData/Common/Models/JsonConfig.cs
@@ -5,12 +5,6 @@ namespace SerializeData
     [Serializable]
     public class JsonConfig
     {
-        public bool IsMultitenancy { get; set; }
-        public bool IsConfirmedAccount { get; set; }
-        public string DefaultConnectionName { get; set; }
-        public string DefaultCompanyName { get; set; }
-        public string DefaultIdentityProvider { get; set; }
-        public string DefaultPlainPassword { get; set; }
         public EmailConfig Email { get; set; }
         public LoginProviderConfig LoginProvider { get; set; }
     }
